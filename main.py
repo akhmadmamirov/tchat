77f06337166c7a1d2f9ff36a2ddc6797f429427a
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-11-02 10:11:26 -0400

    adding memory chain

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain 
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from dotenv import load_dotenv
 
+load_dotenv()
 
+chat = ChatOpenAI()
+memory = ConversationBufferMemory(
+    memory_key="messages",
+    return_messages=True,
+    chat_memory=FileChatMessageHistory("messages.json")
+ )
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template('{content}'),
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory
+)
 
 while True:
     content = input(">>  ")
-    print(f"You entered {content}")
+    
+    result = chain({"content" : content})
+    
+    print(result['text'])
